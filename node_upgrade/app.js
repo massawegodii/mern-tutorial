b727843a8a4773762b84ff19305e884135dffce6
massawegodii <massawegodfrey43@gmail.com>
2024-10-31 17:43:30 +0300

    image uploading ..

--- node_upgrade/app.js
+++ node_upgrade/app.js
@@ -3,17 +3,18 @@ const bodyParser = require('body-parser');
 
 const postsRoute = require('./routes/post');
 const userRoute = require('./routes/user');
-// const imageRoute = require('./routes/images');
+const imageRoute = require('./routes/images');
 // const testRoutes = require('./routes/tests');
 
 const app = express();
 
 app.use(bodyParser.json());
+//Make upload public to be accessed
 app.use('/uploads', express.static('uploads'));
 
 app.use("/posts", postsRoute);
 app.use("/user", userRoute);
-// app.use("/images", imageRoute);
+app.use("/images", imageRoute);
 // app.use("/test", testRoutes);
 
 module.exports = app;
